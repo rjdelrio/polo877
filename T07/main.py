19e8871654716d45b697f447c36c55fdd9e32436
rjdelrio <rjdelrio@uc.cl>
2017-06-23 23:15:50 -0400

    l

--- T07/main.py
+++ T07/main.py
@@ -1,1 +1,31 @@
-print("eres una hij d pt q nnk dv cncr prq ahr v l ntl y nssbl q rs")
+import flask
+import json
+
+
+class Persona:
+    def __init__(self, nombre, rut):
+        self.nombre = nombre
+        self.rut = rut
+        self.id = next(Persona.pid)
+
+    def get_id(self):
+        pid = 0
+        while True:
+            yield pid
+            pid += 1
+
+    pid = get_id()
+
+
+app = flask.Flask(__name__)
+
+# El recurso solo responderá al método GET
+@app.route('/api', methods=['GET'])
+def api_echo():
+    p = Persona('Jason Kruger', '20.000.000-0')
+    resp = flask.Response(json.dumps(p.__dict__), status=200)
+
+    return resp
+
+if __name__ == '__main__':
+    app.run(port=80)  # el servicio estará disponible por el puerto 8080
