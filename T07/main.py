4b064b26ca79c079ad91ff86c095b631de07e000
rjdelrio <rjdelrio@uc.cl>
2017-06-24 23:19:49 -0400

    polo

--- T07/main.py
+++ T07/main.py
@@ -1,31 +1,10 @@
-import flask
-import json
+from flask import Flask
+app = Flask(__name__)
 
 
-class Persona:
-    def __init__(self, nombre, rut):
-        self.nombre = nombre
-        self.rut = rut
-        self.id = next(Persona.pid)
+@app.route('/')
+def hello_world():
+    return 'Hello, World!!!!!'
 
-    def get_id(self):
-        pid = 0
-        while True:
-            yield pid
-            pid += 1
-
-    pid = get_id()
-
-
-app = flask.Flask(__name__)
-
-# El recurso solo responderá al método GET
-@app.route('/api', methods=['GET'])
-def api_echo():
-    p = Persona('Jason Kruger', '20.000.000-0')
-    resp = flask.Response(json.dumps(p.__dict__), status=200)
-
-    return resp
-
-if __name__ == '__main__':
-    app.run(port=80)  # el servicio estará disponible por el puerto 8080
+if __name__ == "__main__":
+    app.run()
